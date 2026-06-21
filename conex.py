92c4b1a964cb2aa92650dcabc6314d738627bbce
Box. <162526757+Aguirre5@users.noreply.github.com>
2026-06-20 22:39:55 -0300

    Add database connection and patient retrieval functions
    xd

---
+++ conex.py
@@ -0,0 +1,20 @@
+import mysql.connector
+
+def conectar():
+    return mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="",
+        database="6hospi"
+    )
+    
+def get_patient():
+    connection = conectar()
+    cursor = connection.cursor()
+    cursor.execute("""
+        SELECT id_paciente, dni, nombre, apellido, fecha_nacimiento, sexo, telefono, estado FROM pacientes
+    """)
+    data = cursor.fetchall()
+    cursor.close()
+    connection.close()
+    return data

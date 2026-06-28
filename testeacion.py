d1d9df6a43b87eee77ba1e964d71fe9d2815b274
Box. <162526757+Aguirre5@users.noreply.github.com>
2026-06-28 18:21:28 -0300

    Delete testeacion.py

--- testeacion.py
+++
@@ -1,9 +0,0 @@
-from prueba import conectar
-
-try:
-    connection = conectar()
-    print("¡¡¡Conexión extosa!!!")
-    connection.close()
-    
-except Exception as e:
-    print("Error al conectar bd.", e)

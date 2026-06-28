5aef0ef7212406a69d274ebc9f4a3e3bca3ef0a4
Box. <162526757+Aguirre5@users.noreply.github.com>
2026-06-28 13:29:12 -0300

    Update SQL queries to use 'fecha_admisión'
    .

--- pacientedb.py
+++ pacientedb.py
@@ -12,7 +12,7 @@ def get_patient():
     connection = conectar()
     cursor = connection.cursor()
     cursor.execute("""
-        SELECT id_paciente, dni, nombre, apellido, fecha_nacimiento, sexo, telefono, fecha_alta, estado FROM pacientes
+        SELECT id_paciente, dni, nombre, apellido, fecha_nacimiento, sexo, telefono, fecha_admisión, estado FROM pacientes
     """)
     data = cursor.fetchall()
     cursor.close()
@@ -24,7 +24,7 @@ def add_paciente(nombre, apellido, dni, fecha_nacimiento, sexo, telefono,):
     connection = conectar ()
     cursor = connection.cursor()
     
-    pdata = """INSERT INTO pacientes (nombre, apellido, dni, fecha_nacimiento, sexo, telefono, fecha_alta, estado) 
+    pdata = """INSERT INTO pacientes (nombre, apellido, dni, fecha_nacimiento, sexo, telefono, fecha_admisión, estado) 
     VALUES (%s, %s, %s, %s, %s, %s, CURDATE(), "Activo")"""
     
     valuen = (nombre, apellido, dni, fecha_nacimiento, sexo, telefono)
